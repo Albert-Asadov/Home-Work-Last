ac04c322abff0fc69573cc3493f6acbbc4d5559e
Albert-Asadov <alberma@code.edu.az>
2022-11-20 20:45:16 +0400

    String Method and class

---
+++ My Home Task/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+#region First Task
+
+//string str = "bU tAsKi eLiYeNe qEdEr cAnIm cIxDi";
+
+//string lower = str.ToLower();
+
+//string[] array = lower.Split();
+
+//string word = string.Empty;
+
+
+
+//foreach (string item in array)
+
+//{
+//    string firstRemove = item.Remove(0, 1);
+
+//    string secondRemove = item.Remove(1);
+
+//    string toUpper = secondRemove.ToUpper();
+
+//    string replace = item.Replace(secondRemove, toUpper);
+
+//    string final = toUpper + firstRemove;
+
+//    word += final + " ";
+
+//}
+//Console.WriteLine(word);
+#endregion
+
+
+
+
+
+
+#region Second Task
+
+using My_Home_Task;
+
+Student_Information information = new Student_Information("Albert", "Asadov", "P230", 85, true);
+
+Console.WriteLine(information.getNameSurname());
+Console.WriteLine(information.allInformation());
+Console.WriteLine(information.canAttendToNextExam()); 
+#endregion
---
+++ My Home Task/Student Information.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Home_Task
+{
+    #region Second Task
+    internal class Student_Information
+    {
+        public string Name;
+        public string SurName;
+        public string Group;
+        public byte Point;
+        public bool IsGraduated;
+
+
+        public Student_Information(string name, string surName, string group, byte point, bool isGraduated)
+        {
+            Name = name;
+            SurName = surName;
+            Group = group;
+            Point = point;
+            IsGraduated = isGraduated;
+        }
+
+
+        public string getNameSurname()
+        {
+            return Name + " " + SurName;
+        }
+
+
+        public string allInformation()
+        {
+            if (IsGraduated)
+            {
+                Console.WriteLine("Bu telebe mezun olub");
+            }
+            else
+            {
+                Console.WriteLine("Mezun olmayib");
+            }
+
+            return Name + ", " + SurName + ", " + Group + ", " + Point;
+        }
+
+        public string canAttendToNextExam()
+        {
+            if (Point > 80)
+            {
+                return "imtahana gire biler";
+            }
+            else
+            {
+                return "imtahana buraxilmir";
+            }
+        }
+    }
+    #endregion
+}
